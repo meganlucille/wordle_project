5311055a8ce400cf4bdedd1020664b4bbac5f14b
meganlucille <megan@mightyjohnsons.com>
2023-09-12 11:45:07 -0600

    Inital commit -  add files

---
+++ Wordle.py
@@ -0,0 +1,24 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+def wordle():
+
+    def enter_action(s):
+        gw.show_message("You have to implement this method.")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
